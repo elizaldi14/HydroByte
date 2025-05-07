2111b87db5404b969592004239c77a8e2609d6ae
EdsonCuevas <efelix@ucol.mx>
2025-05-07 12:22:58 -0600

    insert sensor readings into db

--- utils/serial_reader.py
+++ utils/serial_reader.py
@@ -1,7 +1,6 @@
-# serial_reader.py
-
 import serial
 import time
+import sqlite3
 
 # Diccionario global para guardar los últimos datos leídos
 latest_data = {
@@ -15,8 +14,11 @@ def read_serial_data():
     global latest_data
 
     try:
-        ser = serial.Serial('COM4', 9600, timeout=1)
+        ser = serial.Serial('COM3', 9600, timeout=1)
         time.sleep(2)
+
+        # Guardamos el tiempo de la última inserción
+        last_insert_time = time.time()
 
         while True:
             data_lines = []
@@ -33,8 +35,29 @@ def read_serial_data():
                     latest_data["dist"] = float(data_lines[3])
                 except (IndexError, ValueError):
                     print("Error al procesar los datos recibidos.")
+
             print(latest_data)
+
+            # Verificamos si pasaron 5 minutos (300 segundos)
+            current_time = time.time()
+            if current_time - last_insert_time >= 20:
+                save_to_db()
+                last_insert_time = current_time
+
             time.sleep(5)
 
     except serial.SerialException as e:
         print(f"Error al abrir el puerto serial: {e}")
+
+def save_to_db():
+    conn = sqlite3.connect('hydrobyte.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO sensor_readings (sensor_id, value) VALUES (1, ?)", (latest_data["ph"],))
+    cursor.execute("INSERT INTO sensor_readings (sensor_id, value) VALUES (2, ?)", (latest_data["tds"],))
+    cursor.execute("INSERT INTO sensor_readings (sensor_id, value) VALUES (3, ?)", (latest_data["temp"],))
+    cursor.execute("INSERT INTO sensor_readings (sensor_id, value) VALUES (4, ?)", (latest_data["dist"],))
+
+    conn.commit()
+    conn.close()
+    print("✅ Datos guardados en la base de datos.")
